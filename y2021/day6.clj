d1f6f0ff6fadc2921d135a0bc523a126ec9de806
David B <david.barrutia@reifyhealth.com>
2021-12-06 00:41:56 -0500

    simplify

--- y2021/day6.clj
+++ y2021/day6.clj
@@ -12,32 +12,25 @@
        u/parse-comma-separated-list
        (map parse-long)))
 
-(defn modelate [l]
-  (merge
-   {0 0 1 0 2 0 3 0 4 0 5 0 6 0 7 0 8 0}
-   (frequencies l)))
-
 (defn next [l]
-  {0 (get l 1)
-   1 (get l 2)
-   2 (get l 3)
-   3 (get l 4)
-   4 (get l 5)
-   5 (get l 6)
-   6 (+ (get l 7) (get l 0))
-   7 (get l 8)
-   8 (get l 0)})
- 
+  {0 (get l 1 0)
+   1 (get l 2 0)
+   2 (get l 3 0)
+   3 (get l 4 0)
+   4 (get l 5 0)
+   5 (get l 6 0)
+   6 (+ (get l 7 0) (get l 0 0))
+   7 (get l 8 0)
+   8 (get l 0 0)})
+
 (->> (parse #_"3,4,3,1,2"
             (u/get-input 2021 6))
-     modelate
-
-
+     frequencies
      (iterate next)
      (take (inc 256))
      last
      vals
-     (reduce +)
-     )
-1639854996917
-362639
+     (reduce +))
+
+;; 1639854996917
+;; 362639
